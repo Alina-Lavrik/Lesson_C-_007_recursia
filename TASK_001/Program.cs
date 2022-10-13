d50b877067b1d7b67be4065406cc1a2c571b860f
Alina Lavrik <alina_voro@mail.ru>
2022-10-13 18:19:57 +0300

    Add Task 001_собрать строку с числами от a до b, a <= b

---
+++ TASK_001/Program.cs
@@ -0,0 +1,19 @@
+﻿// Собрать строку с числами от a до b, a <= b
+
+string NumbersFor(int a, int b)
+{
+    string result = String.Empty;
+    for (int i = a; i <= b; i++)  // запускаем цикл, который будет менять счетчит от значения a до <= b
+    {
+        result += $"{i} ";  // в результирующую строку собирвем конкретное значение счетчика
+    }
+    return result;
+}
+string NumbersRec(int a, int b)  // используем рекурсию
+{
+    if (a <= b) return $"{a} " + NumbersRec(a + 1, b); // собираем строку с текущим значением a и вызываем новую копию метода со значениями аргументов
+                                                       // Первый аргумент будет увеличен на 1
+    else return String.Empty;                          // условие окончания рекурсии, возвращаем пустую строку в случае, если условие не выполнилось
+}
+Console.WriteLine(NumbersFor (1, 10));
+Console.WriteLine(NumbersRec (1, 10));
