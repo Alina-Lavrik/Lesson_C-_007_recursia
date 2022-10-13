1999e5e340abd16bc502f095d57a352f77b2f6f3
Alina Lavrik <alina_voro@mail.ru>
2022-10-13 19:47:31 +0300

    Добавили третий вариант решения

--- TASK_005/Program.cs
+++ TASK_005/Program.cs
@@ -12,10 +12,20 @@ int PowerRec(int a, int n)
  if (n == 0) return 1;
  else return PowerRec(a, n - 1) * a;
 }
-
-Console.WriteLine(PowerFor(2, 2));
-Console.WriteLine(PowerRec(2, 10));
-
  // Если показатель степени равен 0 (строка 12), будем возвращать 1, иначе — запустим рекурсивный подсчет
 // там текущее значение а будет домножаться на вызов рекурсивной функции, где в
 // качестве аргумента передаём основания нашей степени PowerRec(a и показатель на 1 меньше.
+
+int PowerRecMath(int a, int n)
+{
+    if (n == 0) return 1;
+    else if (n % 2 == 0) return PowerRecMath(a * a, n / 2);
+    else return PowerRecMath(a, n - 1) * a;
+}
+
+
+Console.WriteLine(PowerFor(2, 2));
+Console.WriteLine(PowerRec(2, 10));
+Console.WriteLine(PowerRecMath(2, 10));
+
+
