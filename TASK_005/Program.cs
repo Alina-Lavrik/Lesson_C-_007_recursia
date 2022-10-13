2b8f844708ac05146162fbfddfa128e625a357b7
Alina Lavrik <alina_voro@mail.ru>
2022-10-13 19:37:29 +0300

    Add Task 005_вычисляем а в степени n

---
+++ TASK_005/Program.cs
@@ -0,0 +1,21 @@
+﻿// Вычислить a в степени n
+
+int PowerFor(int a, int n)
+{
+    int result = 1;
+    for (int i = 1; i <= n; i++) result *= a;
+    return result;
+}
+int PowerRec(int a, int n)
+{
+                                     // return n == 0 ? 1 : PowerRec(a, n - 1) * a
+ if (n == 0) return 1;
+ else return PowerRec(a, n - 1) * a;
+}
+
+Console.WriteLine(PowerFor(2, 2));
+Console.WriteLine(PowerRec(2, 10));
+
+ // Если показатель степени равен 0 (строка 12), будем возвращать 1, иначе — запустим рекурсивный подсчет
+// там текущее значение а будет домножаться на вызов рекурсивной функции, где в
+// качестве аргумента передаём основания нашей степени PowerRec(a и показатель на 1 меньше.
