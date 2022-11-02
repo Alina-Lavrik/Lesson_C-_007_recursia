b9b8d869c3fb7b98c3948c04b862432accb77fda
Alina Lavrik <alina_voro@mail.ru>
2022-11-02 15:27:57 +0300

    Add TASK 008_Folder

---
+++ TASK_008_Folder/Program.cs
@@ -0,0 +1,12 @@
+﻿// Как смотреть содержимое папки
+// когда была создана папка
+string path = "C:/Програмирование/GeekBrains";
+DirectoryInfo di = new DirectoryInfo(path);
+System.Console.WriteLine(di.CreationTime);
+// какие внутри содержатся файлы
+FileInfo[] fi = di.GetFiles();
+
+for (int i = 0; i < fi.Length; i++)
+{
+    System.Console.WriteLine(fi[i].Name);
+}
